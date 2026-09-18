b704dda5d9f240e7aba11cf7cf339a75ed853a81
T.Ruchitha <thumuruchi@gmail.com>
2026-09-18 19:35:34 +0530

    Add basic set operations in sets.py

---
+++ sets.py
@@ -0,0 +1,14 @@
+numbers = {10, 20, 30}
+
+print("Original Set:", numbers)
+
+numbers.add(40)
+print("After Add:", numbers)
+
+numbers.remove(20)
+print("After Remove:", numbers)
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+print("Union:", set1.union(set2))
